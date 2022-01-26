a2e489a9a1c903c349eb562ee5b8c3fbcfff6e55
Rae93 <randyt@rabbit.co.th>
2022-01-26 11:09:55 +0800

    ff

--- main.go
+++ main.go
@@ -20,7 +20,6 @@ type Student struct {
 func Add(a int32, b int32) int32 {
 	if a < 1 {
 		return -a
-
 	}
 	if b < 1 {
 		return -b
@@ -31,21 +30,12 @@ func Add(a int32, b int32) int32 {
 // main function
 func main() {
 
-	// defining an object of struct
 	std1 := Student{"Vani", 94}
-
-	// "New" creates a new template
-	// with name passed as argument
 	tmp1 := template.New("Template_1")
-
-	// "Parse" parses a string into a template
 	tmp1, _ = tmp1.Parse("Hello {{.Name}}, your marks are {{.Marks}}%!")
 
-	// standard output to print merged data
 	err := tmp1.Execute(os.Stdout, std1)
 
-	// if there is no error,
-	// prints the output
 	if err != nil {
 		fmt.Println(err)
 	}
